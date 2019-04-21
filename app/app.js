69f871859d504858354423506c9902c70e32a000
Piotr Woloszun <piotr.woloszun@gmail.com>
2019-04-21 21:53:35 +0200

    material p1

--- app/app.js
+++ app/app.js
@@ -2,13 +2,17 @@
 
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
+  'ngAnimate',
+  'ngAria',
+  'ngMaterial',
+  'ngResource',
   'ngRoute',
+  // app specific
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({ redirectTo: '/view1' });
 }]);
