4b59acbb01f9104bb849560b9a63382ee970a450
Piotr Woloszun <piotr.woloszun@gmail.com>
2019-04-27 22:38:47 +0200

    async modules loader

---
+++ app/app-bootstrap.js
@@ -0,0 +1,20 @@
+'use strict';
+
+window.$script([
+  'lib/angular/angular.js',
+  'lib/angular-animate/angular-animate.js',
+  'lib/angular-aria/angular-aria.js',
+  'lib/angular-material/angular-material.js',
+  'lib/angular-resource/angular-resource.js',
+  'lib/angular-route/angular-route.js',
+  // app
+  'app.js',
+  'view1/view1.js',
+  'view2/view2.js',
+  'core/version/version.js',
+  'core/version/version-directive.js',
+  'core/version/interpolate-filter.js'
+], function () {
+  // when all is done, execute bootstrap AngularJS application
+  angular.bootstrap(window.document, ['myApp']);
+});
